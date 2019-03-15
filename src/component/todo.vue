667378bbe8cf4b8e4a021aa5180d75b1bedbbf9b
0Soo <qhdl301@gmail.com>
2019-03-16 04:15:44 +0900

    20190316 vue 샘플

---
+++ src/component/todo.vue
@@ -0,0 +1,24 @@
+<template>
+    <li>할일 항목 하나입니다. 
+        <span v-if="mode=='type1'">type1 : {{message}} / {{text}} / {{flag}} </span>
+        <span v-else>else : {{message}} / {{text}} / {{count}} </span>
+    </li>
+</template>
+<script>
+     export default {
+       data() { 
+        return {
+          message: 'todo Vue!',
+        } 
+      },
+      methods:{
+
+      },
+      props:[
+          "text",
+          "count",
+          "flag",
+          "mode",
+      ]
+  }
+</script> 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import main from './main.vue'
 
-new Vue({
+ var vueApp = new Vue({
   render: h => h(main)
 }).$mount('#app')
+
+window.vueApp = vueApp;
